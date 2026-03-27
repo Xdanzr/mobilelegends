2e65dd9e8ff2360941fb728262f7e12d49584574
Xdanzr <95982263+Xdanzr@users.noreply.github.com>
2026-03-27 09:44:33 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -125,10 +125,6 @@ document.getElementById("btnSubmitClaim").addEventListener("click", () => {
         alert("ID & Nick wajib diisi!");
         return;
     }
-    if (!/^\d{7,}$/.test(id)) {
-    alert("Claim ID harus terdiri dari minimal 9 angka!");
-    return;
-  }
 
     console.log("CLAIM:", claimData);
 
